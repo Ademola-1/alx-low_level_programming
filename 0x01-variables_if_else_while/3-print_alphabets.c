f305cc76ad4bd813def12ee426d0e7994addb9ba
Ademola-1 <lawalademola71@gmail.com>
2023-09-08 18:50:05 -0700

    A program that alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Prints the alphabet in lowercase and uppercase
+  *can only use putchar
+  *
+  *Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	{
+		putchar(alphabet);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
