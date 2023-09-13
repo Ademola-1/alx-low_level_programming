2bf0de9470fbe1f069ce6d6d0c030e83cf1f37ef
Ademola-1 <lawalademola71@gmail.com>
2023-09-12 17:49:03 -0700

    a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function to print _putchar */
+int _putchar(char);
+#endif
