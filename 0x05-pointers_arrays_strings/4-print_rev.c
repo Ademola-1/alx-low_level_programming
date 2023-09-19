7e76deb68369ccf343c35a38090623d4eca8b0e7
Ademola-1 <lawalademola71@gmail.com>
2023-09-19 16:41:56 -0700

    a function that prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,14 +13,14 @@ void print_rev(char *s)
 	int a, i;
 
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 	for (a = i - 1; a >= 0; a--)
-		{
-			_putchar(s[a]);
-		}
+	{
+		_putchar(s[a]);
+	}
 	_putchar('\n');
 }
