0f910da48fc84f73c444bc64ae55c8b652aa1b03
Ademola-1 <lawalademola71@gmail.com>
2023-09-19 18:31:46 -0700

    a function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _atoi - convert a string to an integer
+  * @s: input
+  * Return: integer
+  */
+int _atoi(char *s)
+{
+	int i, a, con;
+
+	a = 0;
+	con = 1;
+
+	for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+	{
+		if (s[i] == '-')
+		{
+			con = con * -1;
+		}
+	}
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			a = a * 10 + con * (s[i] - '0');
+		if (a != 0 && !(s[i] >= '0' && s[i] <= '9'))
+			return (a);
+	}
+	return (a)
+}
