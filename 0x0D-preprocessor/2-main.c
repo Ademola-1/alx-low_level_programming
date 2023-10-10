30a43c6c768cad01454de9aa9ed7c15603876368
Ademola-1 <lawalademola71@gmail.com>
2023-10-10 14:22:57 -0700

    a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: prints the name of the file it was compiled from
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts(__FILE__);
+	return (0);
+}
