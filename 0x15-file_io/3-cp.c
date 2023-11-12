4ad81ce50ef34fc38271b9a740562aff7de92225
Ademola-1 <lawalademola71@gmail.com>
2023-11-12 06:24:33 -0800

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 	}
 	if (i == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	j = close(fd_rd);
