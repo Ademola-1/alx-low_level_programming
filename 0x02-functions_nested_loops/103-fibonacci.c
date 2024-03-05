86b7003b02209aa5f95150f09a46713a12df6de9
Ademola-1 <lawalademola71@gmail.com>
2024-03-04 22:11:38 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * description: print fibonacci
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	unsigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+	unsigned long int sum = 0;
+
+	while (fib2 <= 4000000)
+	{
+		if (fib2 % 2 == 0)
+		{
+			sum += fib2;
+		}
+		unsigned long int next_fib = fib1 + fib2;
+
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
+
